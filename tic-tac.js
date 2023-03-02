372acb9896d013c4e68b21fab88fb2e40e4b2e3f
Alina Kozyreva <alina0206200119@gmail.com>
2023-03-02 12:10:35 +0300

    Reformat check function

--- tic-tac.js
+++ tic-tac.js
@@ -1,43 +1,23 @@
-const cross = document.getElementById("cross");
-const circle = document.getElementById("circle");
-const field = document.getElementById("field");
-const winner = {
-  circle: "кружочки",
-  cross: "крестики",
-};
-
-const marks = {
-  circle:
-    '<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" fill="#1a0e3b"></path> </svg>',
-  cross:
-    '<svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" fill="#960f08"></path> </svg>',
-};
-
-const winMoves = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 7],
-];
-
-let moves = { circle: [], cross: [] };
-let fields = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let emptyCells = field.querySelectorAll("td").length;
 let activeMark;
 
+function changeMark(mark) {
+  if (mark === "cross") {
+    cross.style.border = "3px solid white";
+    circle.style.border = "";
+  } else {
+    circle.style.border = "3px solid white";
+    cross.style.border = "";
+  }
+  activeMark = mark;
+}
+
 cross.addEventListener("click", () => {
-  cross.style.border = "3px solid white";
-  circle.style.border = "";
-  activeMark = "cross";
+  changeMark("cross");
 });
 
 circle.addEventListener("click", () => {
-  circle.style.border = "3px solid white";
-  cross.style.border = "";
-  activeMark = "circle";
+  changeMark("circle");
 });
 
 field.addEventListener("click", (event) => {
@@ -46,14 +26,13 @@ field.addEventListener("click", (event) => {
     return;
   }
 
-  const move = event.target.getAttribute("value");
-  moves[activeMark].push(+move);
+  if (!event.target.innerHTML && event.target.tagName === "TD") {
+    event.target.innerHTML = marks[activeMark];
+    emptyCells--;
+  }
 
-  drawMark(event.target);
-
-  if (fields.length > 1) {
-    fields = fields.filter((item) => item !== +move);
-    if (checkMove()) {
+  if (emptyCells > 0) {
+    if (checkWin()) {
       swal(`Опа! Победили ${winner[activeMark]} 🎉`);
       setTimeout(clear, 3000);
     }
@@ -61,28 +40,63 @@ field.addEventListener("click", (event) => {
     swal("Ничья! Победила дружба! 🤝");
     setTimeout(clear, 3000);
   }
+
+  changeMark(activeMark === "cross" ? "circle" : "cross");
 });
 
-function drawMark(target) {
-  target.innerHTML = target.innerHTML || marks[activeMark];
-}
-
 function clear() {
-  cells = document.querySelectorAll(".cell");
   for (const item of cells) {
     item.innerHTML = "";
   }
-  fields = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  moves = { circle: [], cross: [] };
+  emptyCells = field.querySelectorAll("td").length;
   circle.style.border = "";
   cross.style.border = "";
   activeMark = "";
 }
 
-function check(el) {
-  return el.every((item) => moves[activeMark].includes(item));
-}
+function checkWin() {
+  let arr = [...Array(len).keys()];
 
-function checkMove() {
-  return winMoves.some(check);
+  for (let i = 0; i < rows.length; i++) {
+    let rowCells = [...rows[i].getElementsByTagName("td")];
+
+    //check rows
+    if (
+      rowCells.every((cell) => cell.innerHTML) &&
+      rowCells.every((cell) => cell.isEqualNode(rowCells[0]))
+    ) {
+      return true;
+    }
+
+    //check columns
+    if (
+      arr.every((index) => rows[index].children[i].innerHTML) &&
+      arr.every((index) =>
+        rows[index].children[i].isEqualNode(rows[i].children[i])
+      )
+    ) {
+      return true;
+    }
+  }
+
+  //check diagonals
+  if (
+    arr.every((index) => rows[index].children[index].innerHTML) &&
+    arr.every((index) =>
+      rows[index].children[index].isEqualNode(rows[0].children[0])
+    )
+  ) {
+    return true;
+  }
+
+  if (
+    arr.every((index) => rows[index].children[len - index - 1].innerHTML) &&
+    arr.every((index) =>
+      rows[index].children[len - index - 1].isEqualNode(
+        rows[0].children[len - 1]
+      )
+    )
+  ) {
+    return true;
+  }
 }
